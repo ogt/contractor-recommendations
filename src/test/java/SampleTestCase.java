d44e6561a45cbef0981f53a626d98a30d97e8cbe
Weichun Qian <sanigo@gmail.com>
2013-03-28 12:11:02 +0800

    Add travis-ci.org CI

---
+++ src/test/java/SampleTestCase.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SampleTestCase {
+
+	@Test
+	public void test1() {
+		Assert.assertTrue(true);
+		System.out.println("Test Message");
+	}
+}

53f98deb9983a4f4dc1a2a3278b9cbbdc3fe5069
Weichun Qian <sanigo@gmail.com>
2013-03-28 13:52:42 +0800

    Add the test to ensure "INFO: Done training" is in output.

--- src/test/java/SampleTestCase.java
+++ src/test/java/SampleTestCase.java
@@ -1,12 +1,40 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class SampleTestCase {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	PrintStream systemOut;
+	PrintStream systemErr;
 
+	@Before
+	public void setUpStreams() {
+		systemOut = System.out;
+		systemErr = System.err;
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(systemOut);
+	    System.setErr(systemErr);
+	}
+	
 	@Test
-	public void test1() {
-		Assert.assertTrue(true);
-		System.out.println("Test Message");
+	public void test1() throws Exception{
+		ContractorRecommendationsTraining.main(new String[]{});
+		System.out.flush();
+		System.err.flush();
+		String output = outContent.toString("UTF-8");
+		String errput = errContent.toString("UTF-8");
+		Assert.assertTrue(errput.indexOf("INFO: Done training") > 0 || output.indexOf("INFO: Done training") > 0);
 	}
 }
